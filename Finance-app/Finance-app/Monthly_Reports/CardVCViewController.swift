dc896f6984bd5ec59d0d63f5a3b0286d77b24a30
Fazil <fazilp300@gmail.com>
2022-09-15 23:46:01 +0400

    Design-tableviewDone

---
+++ Finance-app/Finance-app/Monthly_Reports/CardVCViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CardVCViewController.swift
+//  Finance-app
+//
+//  Created by Fazil P on 15/09/2022.
+//
+
+import UIKit
+
+class CardVCViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Finance-app/Finance-app/Monthly_Reports/CardsLayout.swift
@@ -0,0 +1,64 @@
+//
+//  CardsLayout.swift
+//  Finance-app
+//
+//  Created by Fazil P on 15/09/2022.
+//
+
+import UIKit
+
+
+class CardsLayout: UICollectionViewFlowLayout {
+    
+    
+    private var velocityThresholdPerPage: CGFloat = 2
+    private var numberOfItemsPerPage: CGFloat = 1
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+
+        guard let collectionView = collectionView else { return proposedContentOffset }
+
+        let pageLength: CGFloat
+        let approxPage: CGFloat
+        let currentPage: CGFloat
+        let speed: CGFloat
+
+        if scrollDirection == .horizontal {
+            pageLength = (itemSize.width + minimumLineSpacing) * numberOfItemsPerPage
+            approxPage = collectionView.contentOffset.x / pageLength
+            speed = velocity.x
+        } else {
+            pageLength = (itemSize.height + minimumLineSpacing) * numberOfItemsPerPage
+            approxPage = collectionView.contentOffset.y / pageLength
+            speed = velocity.y
+        }
+
+        if speed < 0 {
+            currentPage = ceil(approxPage)
+        } else if speed > 0 {
+            currentPage = floor(approxPage)
+        } else {
+            currentPage = round(approxPage)
+        }
+
+        guard speed != 0 else {
+            if scrollDirection == .horizontal {
+                return CGPoint(x: currentPage * pageLength, y: 0)
+            } else {
+                return CGPoint(x: 0, y: currentPage * pageLength)
+            }
+        }
+
+        var nextPage: CGFloat = currentPage + (speed > 0 ? 1 : -1)
+
+        let increment = speed / velocityThresholdPerPage
+        nextPage += (speed < 0) ? ceil(increment) : floor(increment)
+
+        if scrollDirection == .horizontal {
+            return CGPoint(x: nextPage * pageLength, y: 0)
+        } else {
+            return CGPoint(x: 0, y: nextPage * pageLength)
+        }
+    }
+}
