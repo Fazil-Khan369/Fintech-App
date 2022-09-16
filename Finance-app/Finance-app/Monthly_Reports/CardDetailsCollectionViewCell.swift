e0c307c2cb65dd97abd13310f4b21a4eeff76624
Fazil <fazilp300@gmail.com>
2022-09-16 12:28:01 +0400

    Design-done

---
+++ Finance-app/Finance-app/Monthly_Reports/CardDetailsCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  CardDetailsCollectionViewCell.swift
+//  Finance-app
+//
+//  Created by Fazil P on 15/09/2022.
+//
+
+import UIKit
+
+class CardDetailsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var viewbackground: UIView!
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var CardnumberLabel: UILabel!
+    @IBOutlet var CardNameLAbel: UILabel!
+    @IBOutlet var ExpiredateLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+}
--- Finance-app/Finance-app/Monthly_Reports/CardVCViewController.swift
+++ Finance-app/Finance-app/Monthly_Reports/CardVCViewController.swift
@@ -11,18 +11,44 @@ class CardVCViewController: UIViewController {
     
     
     
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var cellCardName: UITableViewCell!
+    @IBOutlet var cellAccountnumber: UITableViewCell!
+    @IBOutlet var cellCardholder: UITableViewCell!
+    @IBOutlet var cellExpiredate: UITableViewCell!
+    
+    @IBOutlet var cell3Dsecurity: UITableViewCell!
     
     
+    @IBOutlet var CardNameLabel: UILabel!
+    @IBOutlet var AccountNumberLabel: UILabel!
+    @IBOutlet var CardholderLabel: UILabel!
+    @IBOutlet var ExpiredateLabel: UILabel!
+    @IBOutlet var securityLabel: UILabel!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
 
+    
+    
+    @IBAction func actionReport(_ sender: Any) {
+    }
+    
+    
+    @IBAction func actionTransaction(_ sender: Any) {
+    }
+    
 
-
+    @IBAction func actionActions(_ sender: Any) {
+    }
+    
 
 }
 
