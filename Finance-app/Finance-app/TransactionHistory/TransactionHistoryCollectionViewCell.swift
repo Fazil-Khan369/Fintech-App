6bc035ba279f03a8649d4dcc86cb07d58d5d6539
Fazil <fazilp300@gmail.com>
2022-09-18 09:57:49 +0400

    Design-done

---
+++ Finance-app/Finance-app/TransactionHistory/TransactionHistoryCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TransactionHistoryCollectionViewCell.swift
+//  Finance-app
+//
+//  Created by Fazil P on 18/09/2022.
+//
+
+import UIKit
+
+class TransactionHistoryCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var viewBackground: UIView!
+    @IBOutlet var CardNumberLabel: UILabel!
+    @IBOutlet var Cardview: UIView!
+    @IBOutlet var Cardimage: UIImageView!
+    
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Finance-app/Finance-app/TransactionHistory/TransactionHistoryVC.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionHistoryVC.swift
+//  Finance-app
+//
+//  Created by Fazil P on 17/09/2022.
+//
+
+import UIKit
+
+class TransactionHistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
