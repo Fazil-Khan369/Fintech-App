3a7c31508fad489989dadf4ed31183fae9ca1024
Fazil <fazilp300@gmail.com>
2022-09-19 17:51:10 +0400

    Design-done

---
+++ Finance-app/Finance-app/PaymentLimitVC.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentLimitVC.swift
+//  Finance-app
+//
+//  Created by Fazil P on 19/09/2022.
+//
+
+import UIKit
+
+class PaymentLimitVC: UIViewController {
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+
+}
