d9ffca5c5c06546b8886fa71126104fadd37a45f
Fazil <fazilp300@gmail.com>
2022-09-19 18:08:17 +0400

    Code-done

--- Finance-app/Finance-app/PaymentLimitVC.swift
+++ Finance-app/Finance-app/PaymentLimitVC.swift
@@ -10,17 +10,51 @@ import UIKit
 class PaymentLimitVC: UIViewController {
     
     
+    @IBOutlet var OnlinepaymentLabel: UILabel!
+    @IBOutlet var PaymentinStoreAmount: UILabel!
+    @IBOutlet var CashWithdrawalsAmount: UILabel!
     
     
+    @IBOutlet var OnlinepaymentSlider: UISlider!
+    @IBOutlet var PaymentInStoreAmountSlider: UISlider!
+    @IBOutlet var CashWithdrawalsAmountSlider: UISlider!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+        title = "Limits"
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let userImage = UIImage(systemName: "person.circle.fill")?.withTintColor(UIColor.systemGray, renderingMode: .alwaysOriginal)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: userImage, style: .plain, target: self, action: #selector(actionProfile(_:)))
+        
+        
+        loadData()
         
     }
 
+    func loadData() {
+        
+        OnlinepaymentLabel.text = "$10000.00"
+        PaymentinStoreAmount.text = "$1600"
+        CashWithdrawalsAmount.text = "$2500"
+        
+        OnlinepaymentSlider.value = 0.75
+        PaymentInStoreAmountSlider.value = 0.35
+        CashWithdrawalsAmountSlider.value = 0.55
+    }
+    
+    @objc func actionProfile(_ sender: UIButton) {
+        
+        print(#function)
+    }
 
-
+    @IBAction func actionUpdateLimits(_ sender: Any) {
+        
+        print(#function)
+    }
+    
 }
--- Finance-app/Finance-app/SceneDelegate.swift
+++ Finance-app/Finance-app/SceneDelegate.swift
@@ -11,7 +11,7 @@ import IQKeyboardManagerSwift
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var baseView: Notification!
+    var baseView: PaymentLimitVC!
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
 //        window = UIWindow(frame: UIScreen.main.bounds)
 //
-        baseView = Notification(nibName: "Notification", bundle: nil)
+        baseView = PaymentLimitVC(nibName: "PaymentLimitVC", bundle: nil)
         let navController = NavigationController(rootViewController: baseView)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
