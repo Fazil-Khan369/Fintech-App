f539098e85b5dc9e2e6c3129aaf182b47ecb48f4
Fazil <fazilp300@gmail.com>
2022-09-25 22:10:50 +0400

    Design-done

---
+++ Finance-app/Finance-app/CoinsMarketCell.swift
@@ -0,0 +1,21 @@
+//
+//  CoinsMarketCell.swift
+//  Finance-app
+//
+//  Created by Fazil P on 25/09/2022.
+//
+
+import UIKit
+
+class CoinsMarketCell: UITableViewCell {
+    
+    @IBOutlet var CoinImageview: UIImageView!
+    @IBOutlet var NameLabel: UILabel!
+    @IBOutlet var CoinInfoLabel: UILabel!
+    @IBOutlet var AmountLabel: UILabel!
+    
+    
+
+    
+    
+}
---
+++ Finance-app/Finance-app/CoinsMarketVC.swift
@@ -0,0 +1,29 @@
+//
+//  CoinsMarketVC.swift
+//  Finance-app
+//
+//  Created by Fazil P on 25/09/2022.
+//
+
+import UIKit
+
+class CoinsMarketVC: UIViewController {
+    
+    
+    
+    @IBOutlet var Filtersview: UIView!
+    @IBOutlet var SearchBar: UISearchBar!
+    @IBOutlet var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+ 
+
+}
