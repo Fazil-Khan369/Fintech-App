f102b4aee065b95e98f73c0b706443044ebbdbf2
Fazil <fazilp300@gmail.com>
2022-09-25 23:14:57 +0400

    Code-done

--- Finance-app/Finance-app/CoinsMarketCell.swift
+++ Finance-app/Finance-app/CoinsMarketCell.swift
@@ -15,7 +15,19 @@ class CoinsMarketCell: UITableViewCell {
     @IBOutlet var AmountLabel: UILabel!
     
     
+    func bindData(index: IndexPath, data: [String: String]) {
 
+        guard let name = data["name"] else { return }
+        guard let coin_id = data["coin_id"] else { return }
+        guard let amount = data["amount"] else { return }
+        guard let percentage = data["percentage"] else { return }
+        guard let isPlus = data["isPlus"] else { return }
+
+        CoinImageview.sample("Finance", "Stocks", index.row+10)
+        NameLabel.text = name
+        CoinInfoLabel.text = coin_id + ((isPlus == "Plus") ? " • +" : " • -") + percentage
+        AmountLabel.text = amount
+    }
     
     
 }
