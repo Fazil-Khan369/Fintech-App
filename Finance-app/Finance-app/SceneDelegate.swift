40a06df68c1e31ccf2a6c8c52051859e996b6fd2
Fazil <fazilp300@gmail.com>
2022-09-28 21:39:56 +0400

    Design-code-done

--- Finance-app/Finance-app/SceneDelegate.swift
+++ Finance-app/Finance-app/SceneDelegate.swift
@@ -11,7 +11,7 @@ import IQKeyboardManagerSwift
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var baseView: SettingsVC2!
+    var baseView: AddNewCardVC!
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
 //        window = UIWindow(frame: UIScreen.main.bounds)
 //
-        baseView = SettingsVC2(nibName: "SettingsVC2", bundle: nil)
+        baseView = AddNewCardVC(nibName: "AddNewCardVC", bundle: nil)
         let navController = NavigationController(rootViewController: baseView)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
